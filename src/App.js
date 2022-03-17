c2fb7d5a83433f90bea9adc1e7c154bad2dd1353
kentaouchi <ouchihome0507@gmail.com>
2022-03-17 22:11:40 +0900

    add.materialUI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import './App.css';
 
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+export default function BasicButtons() {
+  return (
+    <Stack spacing={2} direction="row">
+      <Button variant="text">実行</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+  );
+}
+
 function App() {
   return (
     <div className="App">
@@ -7,10 +21,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+       
       
       </header>
     </div>
   );
 }
 
-export default App;
